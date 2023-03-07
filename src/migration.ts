587e259c82ad2b28442b2a1d8200f0c54155def7
Jan Malcánek <jan@malcanek.cz>
2023-03-07 16:38:50 +0100

    Allow using raw queries and values for migrations, not only MigrationCommand

--- src/migration.ts
+++ src/migration.ts
@@ -1,17 +1,36 @@
 import { query } from './migrationManager';
 import { MigrationCommand } from './migrationSqlBuilder';
-export class Migration {
-  private _migrationCommand: MigrationCommand;
 
-  constructor(migrationCommand: MigrationCommand) {
+interface IMigrationQuery {
+  up: {
+    query: string;
+    binds?: any[];
+  },
+  down: {
+    query: string;
+    binds?: any[];
+  }
+}
+export class Migration {
+  private _migrationCommand: MigrationCommand | IMigrationQuery;
+
+  constructor(migrationCommand: MigrationCommand | IMigrationQuery) {
     this._migrationCommand = migrationCommand;
   }
 
   async up(query: query) {
-    await query(this._migrationCommand.getUpSql())
+    if(this._migrationCommand instanceof MigrationCommand) {
+      await query(this._migrationCommand.getUpSql())
+    } else {
+      await query(this._migrationCommand.up.query, this._migrationCommand.up.binds);
+    }
   }
 
   async down(query: query) {
-    await query(this._migrationCommand.getDownSql());
+    if(this._migrationCommand instanceof MigrationCommand) {
+      await query(this._migrationCommand.getDownSql());
+    } else {
+      await query(this._migrationCommand.down.query, this._migrationCommand.down.binds);
+    }
   }
 }
